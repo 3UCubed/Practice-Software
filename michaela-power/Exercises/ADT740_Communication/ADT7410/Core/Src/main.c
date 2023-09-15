07a62ab1e8437adf0af849a8580bce434d297f4d
mrp1106@wildcats.unh.edu <mrp1106@wildcats.unh.edu>
2023-09-15 13:03:26 -0400

    added TX back to code

--- michaela-power/Exercises/ADT740_Communication/ADT7410/Core/Src/main.c
+++ michaela-power/Exercises/ADT740_Communication/ADT7410/Core/Src/main.c
@@ -82,27 +82,30 @@ int main(void)
 	  buffer[0] = REG_TMP;
 
 	      //RECEIVE DATA
-	  	  	ret = HAL_I2C_Master_Receive(&hi2c1, ADT7410_ADDR, buffer, 2, HAL_MAX_DELAY);
-	      	if (ret != HAL_OK) {
-	      		if (HAL_I2C_GetError(&hi2c1) != HAL_I2C_ERROR_AF){
-	      			Error_Handler(); //BASIC ERROR CHECK
-	      		}
-	      		//IF ERROR PRODUCED DURING RECEIVAL
-	      		strcpy(uart, "ERROR RX\r\n");
-	      	} else {
-	      		//CONVERT DATA TO CELSIUS (WORKS)
-	      		final_data = ((uint16_t)buffer[0]<<8)|(buffer[1]) >> 3;
-	      		if (final_data>511) { //MSB is 1, negative case
-	      			final_data = (final_data-8192)/16;
-	      		} else { //MSB is 0, positive case
-	      			final_data = (final_data)/16;
-	      		}
-	      		val = final_data*100;
-	      		//FORMAT INTO DIGESTABLE STRING
-	      		large = (unsigned int)val/100;
-	      		small = (unsigned int)val%100;
-	      		sprintf(uart, "%u.%02u C\r\n", large, small);
-	      	}
+	  ret =  HAL_I2C_Master_Transmit(&hi2c1, ADT7410_ADDR, buffer, 1, HAL_MAX_DELAY);
+	  if (ret != HAL_OK) {
+		  strcpy(uart, "ERROR TX\r\n");
+	  } else {
+		  ret = HAL_I2C_Master_Receive(&hi2c1, ADT7410_ADDR, buffer, 2, HAL_MAX_DELAY);
+		  if (ret != HAL_OK) {
+		  	 //IF ERROR PRODUCED DURING RECEIVAL
+		  	 strcpy(uart, "ERROR RX\r\n");
+		  } else{
+			  //CONVERT DATA TO CELSIUS (WORKS)
+			  final_data = ((uint16_t)buffer[0]<<8)|(buffer[1]) >> 3;
+			  if (final_data>511) { //MSB is 1, negative case
+			  	  final_data = (final_data-8192)/16;
+			  } else { //MSB is 0, positive case
+			  	  final_data = (final_data)/16;
+			  }
+			  val = final_data*100;
+			  //FORMAT INTO DIGESTABLE STRING
+			  large = (unsigned int)val/100;
+			  small = (unsigned int)val%100;
+			  sprintf(uart, "%u.%02u C\r\n", large, small);
+		  }
+	  }
+
 	      //TRANSMIT STRING IN PROPER FORM TO PC via UART
 	      HAL_UART_Transmit(&huart1, (uint8_t*)uart, strlen(uart), HAL_MAX_DELAY);
 	      HAL_Delay(1000);
