8a3b8940f147cb49a2400b421d883e71de474922
siamanna <Sia.Manna@unh.edu>
2024-06-28 16:24:15 -0400

    finished cleaning encoder_mod

--- sia-manna/ax25decoder/encoder_mod.py
+++ sia-manna/ax25decoder/encoder_mod.py
@@ -12,26 +12,22 @@ def convert_msb_to_lsb(buffer):
     return bytes(reverse_bits(byte) for byte in buffer)
 
 def crc_calc(buffer, size_frame):
-    shiftRegister = 0xFFFF  # Initialization of the Shift Register to 0xFFFF
+    shift_register = 0xFFFF  # Initialization of the Shift Register to 0xFFFF
 
     for i in range(size_frame):  # Process all bytes in the frame
         byte = buffer[i]
-
         for j in range(8):  # Process each bit from LSB to MSB
-            outBit = shiftRegister & 0x0001  # Get the LSB of the shift register
-            shiftRegister >>= 1  # Shift the register to the right
-
-            if outBit != (byte & 0x01):  # Compare LSB of shift register and current bit of byte
-                shiftRegister ^= 0x8408  # XOR with the mirrored polynomial if the bits are different
-            
+            out_bit = shift_register & 0x0001  # Get the LSB of the shift register
+            shift_register >>= 1  # Shift the register to the right
+            if out_bit != (byte & 0x01):  # Compare LSB of shift register and current bit of byte
+                shift_register ^= 0x8408  # XOR with the mirrored polynomial if the bits are different
             byte >>= 1  # Shift the byte to the right to process the next bit
 
-    return shiftRegister ^ 0xFFFF  # Final XOR.
+    return shift_register ^ 0xFFFF  # Final XOR
 
 def bit_stuffing(data):
     stuffed_data = []
     count = 0
-    
     for bit in data:
         stuffed_data.append(bit)
         if bit == '1':
@@ -41,7 +37,6 @@ def bit_stuffing(data):
                 count = 0
         else:
             count = 0
-    
     return ''.join(stuffed_data)
 
 def construct_ax25_frame():
